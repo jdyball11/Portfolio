5fc7486f21b2fd0976559d57cbbb309dc76eed79
jdyball11 <jack.dyball92@gmail.com>
2023-01-19 03:20:42 +1100

    changed pth to Home in app.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar'
-import Home from './components/Home'
+import Home from '../src/components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Contact from './components/Contact'
+
 import './App.css'
 
 function App() {
